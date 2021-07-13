29389fb8a9160dddb0effe21bd980d3810a0e9bf
JoinChen <cjztan@foxmail.com>
2021-07-13 23:02:12 +0800

    feat:柯里化

---
+++ 04-JavaScript函数式编程/code/05.js
@@ -0,0 +1,5 @@
+var count = 20;
+function checkSize(num) {
+    return num > count;
+}
+console.log(checkSize(10));
---
+++ 04-JavaScript函数式编程/code/06.js
@@ -0,0 +1,6 @@
+let count = 20;
+function checkSize(num: number): boolean {
+    return num > count;
+}
+
+console.log(checkSize(10))
